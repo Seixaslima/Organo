02bd7d0005fe7bc92b084f77111ec42b621bd7d8
Lucas Seixas <lucasseixaslima2@gmail.com>
2023-06-11 13:46:16 -0300

    exercicios finalizados unidade 1

---
+++ src/componentes/Formulario/index.js
@@ -0,0 +1,16 @@
+import './Formulario.css';
+import CampoTexto from '../CampoTexto';
+
+const Formulario = () => {
+  return (
+    <section className="formulario">
+      <form>
+        <CampoTexto label="Nome" placeholder="Digite seu nome" />
+        <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
+        <CampoTexto label="Imagem" placeholder="Informe o endereço da imagem" />
+      </form>
+    </section>
+  );
+};
+
+export default Formulario;
