3d2d0d0f4baad86bde48101d735c72c243325de1
Lucas Seixas <lucasseixaslima2@gmail.com>
2023-07-07 14:05:30 -0300

    unidade 2 finalizada

--- src/componentes/Campo/index.tsx
+++ src/componentes/Campo/index.tsx
@@ -1,5 +1,14 @@
 import './Campo.css';
 
+interface CampoProps {
+  type: string
+  label: string
+  placeholder: string
+  valor: string
+  aoAlterado: (valor: string) => void
+  obrigatorio: boolean
+}
+
 const Campo = ({
   type = 'text',
   label,
@@ -7,7 +16,7 @@ const Campo = ({
   valor,
   aoAlterado,
   obrigatorio = false
-}) => {
+}: CampoProps) => {
   return (
     <div className={`campo campo-${type}`}>
       <label>{label}</label>
